a62212bca1bfefabb982ff3e07c9e1058d8356c8
Notford <kwame.notford@gmail.com>
2023-02-20 23:49:40 +0000

    Updates

--- index.js
+++ index.js
@@ -11,20 +11,37 @@ $(window).on("hashchange", function () {
 });
 $(window).trigger("hashchange");
 
+/*
+function auth(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	if(email== "admin@gmail.com" && password == "admin123") {
+		window.location.assign("homepage.html");
+		alert("Login Successfully!");  
+	}
+	else {
+		alert("Invalid Credentials");
+		return;
+	}
+}
+*/
+
 function validateLoginForm() {
 	var name = document.getElementById("logName").value;
 	var password = document.getElementById("logPassword").value;
+	
 
 	if (name == "" || password == "") {
 		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
-		return false;
+		return true;
 	}
 
-	else if (password.length < 8) {
+	else if (password.length < 8 || name == "" || password == "" ) {
 		document.getElementById("errorMsg").innerHTML = "Your password must include at least 8 characters"
 		return false;
 	}
 	else {
+		window.location.assign("homepage.html");
 		alert("Successfully logged in");
 		return true;
 	}
@@ -44,8 +61,8 @@ function validateSignupForm() {
 		return false;
 	}
 	else {
+		window.location.assign("homepage.html");
 		alert("Successfully signed up");
 		return true;
 	}
 }
-
